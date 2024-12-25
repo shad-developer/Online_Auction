e67e61f48f2ad48a14f037d7f7e32bfab2ca6569
Shad-developer <muhammadumairshad22@gmail.com>
2024-12-25 11:51:06 +0500

    first commit

---
+++ Client/src/assets/data.js
@@ -0,0 +1,235 @@
+export const User1 = "https://cdn-icons-png.flaticon.com/128/6997/6997662.png";
+export const user2 = "https://cdn-icons-png.flaticon.com/128/236/236832.png";
+export const user3 = "https://cdn-icons-png.flaticon.com/128/236/236131.png";
+export const user4 = "https://cdn-icons-png.flaticon.com/128/1154/1154448.png";
+
+export const menuList = [
+  {
+    id: 1,
+    path: "/",
+    link: "Home",
+  },
+  {
+    id: 2,
+    path: "/inbox",
+    link: "Inbox",
+  },
+  {
+    id: 3,
+    path: "/products",
+    link: "Browse Products",
+  },
+  {
+    id: 4,
+    path: "/about",
+    link: "About Us",
+  },
+  {
+    id: 5,
+    path: "/contact",
+    link: "Contact Us",
+  },
+];
+
+export const categorylists = [
+  {
+    id: 1,
+    image: "../images/category/c1.png",
+    title: "watches",
+  },
+  {
+    id: 2,
+    image: "../images/category/c2.png",
+    title: "electronics",
+  },
+  {
+    id: 3,
+    image: "../images/category/c3.png",
+    title: "sports",
+  },
+  {
+    id: 4,
+    image: "../images/category/c6.png",
+    title: "jewelry",
+  },
+  {
+    id: 5,
+    image: "../images/category/c7.png",
+    title: "clothes",
+  },
+];
+
+export const processList = [
+  {
+    id: "01",
+    title: "Sign up",
+    desc: "Sign your up via our contact form or via the Register Form on the website. or Send us a couple of pictures of the product that you want to put up for auction.",
+    cover:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/shape-7.png",
+  },
+  {
+    id: "02",
+    title: "Auction goes online",
+    desc: "As soon as we collect enough collections for the auction (we strive for 25 to 40 items per auction), the auction will show up on the website.",
+    cover:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/auction.png",
+  },
+  {
+    id: "03",
+    title: "Closing auction",
+    desc: "After the viewing day(s) the auction is still a few days open for new bids. We evaluate the highest bid after the closing of an auction.",
+    cover:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/auction-2.png",
+  },
+  {
+    id: "04",
+    title: "The last steps",
+    desc: "After the item is sold to the highest bidder, all the items will be collected by the buyers on a determined date.",
+    cover:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/auction-3.png",
+  },
+];
+
+export const topSellerList = [
+  {
+    id: 1,
+    title: "William",
+    profile:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/client-5-1.png",
+    amount: "100",
+  },
+  {
+    id: 2,
+    title: "Orko",
+    profile:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/client-6.png",
+    amount: "200",
+  },
+  {
+    id: 3,
+    title: "Nipa",
+    profile:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/client-3.png",
+    amount: "300",
+  },
+  {
+    id: 4,
+    title: "Joseph",
+    profile:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/client-2-2.png",
+    amount: "100",
+  },
+  {
+    id: 5,
+    title: "Isabella",
+    profile:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/client-10-2.png",
+    amount: "100",
+  },
+  {
+    id: 6,
+    title: "Emily",
+    profile:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/client-15-1.png",
+    amount: "100",
+  },
+  {
+    id: 7,
+    title: "Devmahbub",
+    profile:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/client-8-3.png",
+    amount: "100",
+  },
+  {
+    id: 8,
+    title: "Ava Garcia",
+    profile:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/client-4-1.png",
+    amount: "100",
+  },
+  {
+    id: 9,
+    title: "Amelia Harris",
+    profile:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/client-13.png",
+    amount: "100",
+  },
+  {
+    id: 10,
+    title: "Alexander",
+    profile:
+      "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/01/client-8.png",
+    amount: "100",
+  },
+];
+
+export const trustList = [
+  {
+    id: 1,
+    profile: "https://bidout-react.vercel.app/images/bg/sponsor1.png",
+  },
+  {
+    id: 2,
+    profile: "https://bidout-react.vercel.app/images/bg/sponsor3.png",
+  },
+  {
+    id: 3,
+    profile: "https://bidout-react.vercel.app/images/bg/sponsor4.png",
+  },
+  {
+    id: 4,
+    profile: "https://bidout-react.vercel.app/images/bg/sponsor5.png",
+  },
+  {
+    id: 5,
+    profile: "https://bidout-react.vercel.app/images/bg/sponsor6.png",
+  },
+  {
+    id: 6,
+    profile: "https://bidout-react.vercel.app/images/bg/sponsor8.png",
+  },
+  {
+    id: 7,
+    profile: "https://bidout-react.vercel.app/images/bg/sponsor9.png",
+  },
+
+];
+
+export const topList = [
+  {
+    id: 1,
+    catgeory: "Electronics",
+    total: 7,
+    img1: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-01-768x768.jpg",
+    img2: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-04-150x150.jpg",
+    img3: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-07-150x150.jpg",
+    img4: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-03-150x150.jpg",
+  },
+  {
+    id: 2,
+    catgeory: "PC Components",
+    total: 10,
+    img1: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-07-768x768.jpg",
+    img2: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-02.jpg",
+    img3: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-10-150x150.jpg",
+    img4: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-04-150x150.jpg",
+  },
+  {
+    id: 3,
+    catgeory: "Audio Eqiupments",
+    total: 8,
+    img1: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-01-768x768.jpg",
+    img2: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-03-150x150.jpg",
+    img4: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-05-150x150.jpg",
+    img3: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/electronics-product-08-150x150.jpg",
+  },
+  {
+    id: 4,
+    catgeory: "Automotive",
+    total: 4,
+    img3: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/automotive_product-05-300x200.jpg",
+    img2: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/automotive_product-05-768x768.jpg",
+    img4: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/automotive_product-07-150x150.jpg",
+    img1: "https://rainbowthemes.net/themes/nuron/wp-content/uploads/2023/09/automotive_product-04-768x768.jpg",
+  },
+];
